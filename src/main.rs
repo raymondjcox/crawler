779e1ac41ef4348b9c419e86ad98e3a24411c437
Raymond Cox <raymond@raymondjcox.com>
2021-03-27 22:03:48 -0400

    code could be cleaner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use url::Url;
 use std::collections::HashSet;
 use futures::future::join_all;
 use regex::Regex;
@@ -20,31 +21,41 @@ fn get_links(html: &str) -> Vec<String> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let banned = ["linkedin", "twitter", "instagram", "facebook", "drift", "github"];
+    let banned = ["linkedin", "twitter", "instagram", "facebook", "drift", "github", "google", "aws", "wikipedia"];
     let mut visited_urls = HashSet::new();
     let mut urls: Vec<String> = vec![String::from("https://jiayiwu.me")];
 
     while !urls.is_empty() {
         let mut tasks = Vec::new();
+        println!("Number of sites analyzed: {}", visited_urls.len());
         while !urls.is_empty() {
             let url = urls.pop().unwrap();
-            println!("visiting {}", url);
             visited_urls.insert(url.clone());
             tasks.push(task::spawn(get_html(url)));
         }
         for html in join_all(tasks).await {
-            let h = html.unwrap().unwrap();
-            let url = h.0;
-            let links = get_links(&h.1);
-            // println!("found these links {:?}", links);
-            for link in links {
-                if link.starts_with("http") && !visited_urls.contains(&link) && !banned.iter().any(|b| link.contains(b)) {
-                    urls.push(link.clone());
-                }
-
-                if !link.starts_with("http") {
-                    urls.push(format!("{}{}", url, link));
-                }
+            let h = html.unwrap();
+            match h {
+                Ok((url, html)) => {
+                    println!("Finding links for {}", url);
+                    let links = get_links(&html);
+                    for link in links {
+                        if visited_urls.contains(&link) || banned.iter().any(|b| link.contains(b)) {
+                            continue;
+                        }
+                        if link.starts_with("http") {
+                            urls.push(link.clone());
+                        } else {
+                            let parsed_url = String::from(Url::parse(&url).unwrap().join(&link).unwrap().as_str());
+                            if visited_urls.contains(&parsed_url) {
+                                continue;
+                            }
+                            visited_urls.insert(parsed_url.clone());
+                            urls.push(parsed_url);
+                        }
+                    }
+                },
+                Err(_a) => continue
             }
         }
     }
