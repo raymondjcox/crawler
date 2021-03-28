83c2d175b6b1e9d4e680fc6f7ef19ff9fdfb62ef
Raymond Cox <raymond@raymondjcox.com>
2021-03-28 14:55:08 -0400

    Make things nicer & scope to url

--- src/main.rs
+++ src/main.rs
@@ -4,58 +4,55 @@ use futures::future::join_all;
 use regex::Regex;
 use tokio::task;
 
-async fn get_html(url: String) -> Result<(String, String), Box<dyn std::error::Error + Send + Sync>> {
+const DOMAIN: &str = "google.com";
+const START_URL: &str = "https://www.google.com";
+
+fn get_links_from_html(url: &str, html: &str) -> Vec<String> {
+    let parsed_url = Url::parse(&url).unwrap();
+    let anchor_tags = Regex::new(r#"a href="(.*?)""#).unwrap();
+    return anchor_tags.captures_iter(&html).map(|cap| String::from(parsed_url.join(cap.get(1).unwrap().as_str()).unwrap().as_str())).collect()
+}
+
+
+async fn get_links(url: String) -> Result<Vec<String>, Box<dyn std::error::Error + Send + Sync>> {
     let html = reqwest::get(url.clone())
         .await?
         .text()
         .await?;
-    Ok((url, html))
+    let links = task::spawn_blocking(move ||get_links_from_html(&url, &html)).await?;
+    Ok(links)
 }
-
-
-fn get_links(html: &str) -> Vec<String> {
-    let anchor_tags = Regex::new(r#"a href="(.*?)""#).unwrap();
-    return anchor_tags.captures_iter(&html).map(|cap| String::from(cap.get(1).unwrap().as_str())).collect()
-}
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let banned = ["linkedin", "twitter", "instagram", "facebook", "drift", "github", "google", "aws", "wikipedia"];
     let mut visited_urls = HashSet::new();
-    let mut urls: Vec<String> = vec![String::from("https://jiayiwu.me")];
+    let mut num_visited = 0;
+    let mut urls: Vec<String> = vec![String::from(START_URL)];
 
     while !urls.is_empty() {
         let mut tasks = Vec::new();
-        println!("Number of sites analyzed: {}", visited_urls.len());
-        while !urls.is_empty() {
-            let url = urls.pop().unwrap();
-            visited_urls.insert(url.clone());
-            tasks.push(task::spawn(get_html(url)));
+        let mut urls_to_visit = Vec::new();
+        println!("Number of sites visited: {}", num_visited);
+        while !urls.is_empty() && tasks.len() < 8 {
+            let url = urls.remove(0);
+            urls_to_visit.push(url.clone());
+            tasks.push(task::spawn(get_links(url)));
+            num_visited += 1;
         }
-        for html in join_all(tasks).await {
-            let h = html.unwrap();
-            match h {
-                Ok((url, html)) => {
-                    println!("Finding links for {}", url);
-                    let links = get_links(&html);
+        println!("Visiting {} urls: {:?}", urls_to_visit.len(), urls_to_visit);
+
+        for links in join_all(tasks).await {
+            match links.unwrap() {
+                Ok(links) => {
                     for link in links {
-                        if visited_urls.contains(&link) || banned.iter().any(|b| link.contains(b)) {
+                        if visited_urls.contains(&link) || !link.contains(DOMAIN) {
                             continue;
                         }
-                        if link.starts_with("http") {
-                            urls.push(link.clone());
-                        } else {
-                            let parsed_url = String::from(Url::parse(&url).unwrap().join(&link).unwrap().as_str());
-                            if visited_urls.contains(&parsed_url) {
-                                continue;
-                            }
-                            visited_urls.insert(parsed_url.clone());
-                            urls.push(parsed_url);
-                        }
+                        visited_urls.insert(link.clone());
+                        urls.push(link);
                     }
                 },
-                Err(_a) => continue
+                Err(e) => println!("{:?}", e)
             }
         }
     }
