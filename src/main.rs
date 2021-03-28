4fbb78e4c8002a8a5ca51e1670b5059604983b3b
Raymond Cox <raymond@raymondjcox.com>
2021-03-27 21:26:58 -0400

    meh it's okay

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,52 @@
+use std::collections::HashSet;
 use futures::future::join_all;
+use regex::Regex;
 use tokio::task;
 
-async fn get_html(url: &str) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
-    let res = reqwest::get(url)
+async fn get_html(url: String) -> Result<(String, String), Box<dyn std::error::Error + Send + Sync>> {
+    let html = reqwest::get(url.clone())
         .await?
         .text()
         .await?;
-    Ok(res)
+    Ok((url, html))
 }
+
+
+fn get_links(html: &str) -> Vec<String> {
+    let anchor_tags = Regex::new(r#"a href="(.*?)""#).unwrap();
+    return anchor_tags.captures_iter(&html).map(|cap| String::from(cap.get(1).unwrap().as_str())).collect()
+}
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let visited_url
-    let urls = ["https://raymondjcox.com", "https://jiayiwu.me"];
-    let mut tasks = vec![];
-    for url in &urls {
-        tasks.push(task::spawn(get_html(url)));
-    }
-    // let result = join_all(tasks).await;
-    for res in join_all(tasks).await {
-        println!("{:?}", res);
+    let banned = ["linkedin", "twitter", "instagram", "facebook", "drift", "github"];
+    let mut visited_urls = HashSet::new();
+    let mut urls: Vec<String> = vec![String::from("https://jiayiwu.me")];
+
+    while !urls.is_empty() {
+        let mut tasks = Vec::new();
+        while !urls.is_empty() {
+            let url = urls.pop().unwrap();
+            println!("visiting {}", url);
+            visited_urls.insert(url.clone());
+            tasks.push(task::spawn(get_html(url)));
+        }
+        for html in join_all(tasks).await {
+            let h = html.unwrap().unwrap();
+            let url = h.0;
+            let links = get_links(&h.1);
+            // println!("found these links {:?}", links);
+            for link in links {
+                if link.starts_with("http") && !visited_urls.contains(&link) && !banned.iter().any(|b| link.contains(b)) {
+                    urls.push(link.clone());
+                }
+
+                if !link.starts_with("http") {
+                    urls.push(format!("{}{}", url, link));
+                }
+            }
+        }
     }
     Ok(())
 }
